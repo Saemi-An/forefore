3dc6a703be5bc60a49c97c2f7fcda720b836c545
Saemi <cchocolatec@naver.com>
2025-06-17 17:23:33 +0900

    도커, 도커 컴포즈, 엔트리포인트 추가

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# 에러 발생시 멈춤
+set -e
+
+RUN_MANAGE_PY='poetry run python -m core.manage'
+
+echo '장고 정적파일 수집중 ...'
+$RUN_MANAGE_PY collectstatic --no-input
+
+echo '장고 마이그레이션 진행중 ...'
+$RUN_MANAGE_PY migrate --no-input
+
+echo '구니콘 시작중..'
+# /gunicorn 디렉토리가 존재하는 상태에서 gunicorn.sock 자동 생성됨
+# 컨테이너 내부 경로 기준으로 작성된 path이기 때문에 컴포즈에서 ./shared/gunicorn와 연결됨
+exec poetry run gunicorn core.project.wsgi:application \
+    --bind unix:/gunicorn/gunicorn.sock \
+    --workers 3
